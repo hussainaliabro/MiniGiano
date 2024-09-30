631b2e0bb86422471b017ee9e68ff72589fcf6b4
hussainaliabro <hussainaliabro52@gmail.com>
2024-09-30 17:50:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ function startGame(game) {
         window.location.href = 'tictactoe.html'; // Redirect to Tic Tac Toe game
     }
 }
+async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4870434982079852"
+     crossorigin="anonymous"
