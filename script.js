57fcbb7c217cc48d7ce589171666b154090ab9bc
hussainaliabro <hussainaliabro52@gmail.com>
2024-09-30 17:08:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function startGame(game) {
+    if (game === 'snake') {
+        window.location.href = 'snake.html'; // Redirect to Snake game
+    } else if (game === 'tictactoe') {
+        window.location.href = 'tictactoe.html'; // Redirect to Tic Tac Toe game
+    }
+}
